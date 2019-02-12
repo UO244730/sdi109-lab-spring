2e874d9bfbf49ad8eb210516b584715f493ad1a8
UO244730 <UO244730@uniovi.es>
2019-02-12 10:57:20 +0100

    Creación inicial de proyecto Notaneitor

---
+++ src/main/java/com/uniovi/Sdi109LabSpringApplication.java
@@ -0,0 +1,14 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi109LabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi109LabSpringApplication.class, args);
+	}
+
+}
+
