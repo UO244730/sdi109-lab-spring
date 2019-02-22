a17ea9fedb043be32e8061c22a9bf346094436a1
UO244730 <UO244730@uniovi.es>
2019-02-22 12:01:05 +0100

    SDI-SpringBoot-Sesión-4.3-Control de acceso por roles

--- src/main/java/com/uniovi/entities/Mark.java
+++ src/main/java/com/uniovi/entities/Mark.java
@@ -9,6 +9,7 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
+	private Boolean resend = false;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
@@ -66,6 +67,14 @@ public class Mark {
 	@Override
 	public String toString() {
 		return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
+	}
+
+	public Boolean getResend() {
+		return resend;
+	}
+
+	public void setResend(Boolean resend) {
+		this.resend = resend;
 	}
 
 }
